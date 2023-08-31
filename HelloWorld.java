c84b009f41e6fc8325680c975e8a49a4ff48bc8e
Sariah Adams <108815131+sariahadams@users.noreply.github.com>
2023-08-31 14:44:56 -0400

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/**
+		 * Sariah Adams
+		 */
+		
+		System.out.println("Hello World!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
