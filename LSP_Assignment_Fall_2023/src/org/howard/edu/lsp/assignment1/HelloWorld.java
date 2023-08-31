113118fa17ab0a47bfe72938a6d2771cc34b7dda
sariahadams <adamssariah2@gmail.com>
2023-08-31 17:28:06 -0400

    Initial commit

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	/**
+	 * Sariah Adams
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
