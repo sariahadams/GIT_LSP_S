3375c2d6c851fe4d569b8eabfde803773ea33169
sariahadams <adamssariah2@gmail.com>
2023-12-07 20:01:00 -0500

    First commit

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/exam/problem51/TestLogger.java
@@ -0,0 +1,41 @@
+package org.howard.edu.lsp.exam.problem51;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class TestLogger {
+
+    @Test
+    public void testDisplayLog() {
+        Logger logger = Logger.getInstance();
+        logger.logMessage("Test Message");
+        assertEquals("Log:\nTest Message\n", logger.displayLog());
+    }
+
+    @Test
+    public void testLoggerInstances() {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+        assertSame(logger1, logger2);
+    }
+
+    @Test
+    public void testMultipleLogMessages() {
+        Logger logger = Logger.getInstance();
+        logger.logMessage("Message1:");
+        logger.logMessage("Message 2:");
+        
+        // Get the log after logging messages
+        String log = logger.displayLog();
+        
+        // Assertion 1: Check if Message1 is logged
+        assertEquals(true, log.contains("Message1:"));
+        
+        // Assertion 2: Check if Message 2 is logged after Message1
+        assertEquals(true, log.contains("Message 2:"));
+    }
+
+}
+
+
